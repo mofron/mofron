121cf0312496887590710d36256c7c0ba15a9273
simpart <simpart>
2016-10-21 15:31:27 +0900

    move id gen

--- src/mofron.js
+++ src/mofron.js
@@ -14,8 +14,8 @@ try {
             effect    : {}    ,
             other     : {}    ,
             rootConts : null  ,
-            useParts  : null  
-            
+            useParts  : null  ,
+            innerFunc : {}
         };
         mofron.init     = function(bp, cb) {
             try {
@@ -74,6 +74,28 @@ try {
                 mofron.js_loader.Seri.load();
             } catch (e) {
                 console.error(e.stack);
+            }
+        };
+        mofron.innerFunc.getId = function () {
+            try {
+                var ret_id = "";
+                var loop   = 0;
+                var val    = 0;
+                for (loop=0; loop < 32 ;loop++) {
+                    val = Math.random() * 16 | 0;
+                    if ((loop === 8)  ||
+                        (loop === 12) ||
+                        (loop === 16) ||
+                        (loop === 20)) {
+                        ret_id += "-";
+                    }
+                    ret_id += (loop == 12 ? 4 : (loop == 16 ? (val & 3 | 8) : val)).toString(16);
+                }
+                this.id = ret_id;
+                return ret_id;
+            } catch (e) {
+                console.error(e.stack);
+                throw new Error();
             }
         };
     } else {
--- src/parts/base/Component.js
+++ src/parts/base/Component.js
@@ -33,21 +33,8 @@ mofron.parts.Component = class {
         if (null != this.id) {
             return this.id;
         }
-        var ret_id = ""; 
-        var loop   = 0; 
-        var val    = 0;
-        for (loop=0; loop < 32 ;loop++) {
-            val = Math.random() * 16 | 0;
-            if ((loop === 8)  ||
-                (loop === 12) ||
-                (loop === 16) ||
-                (loop === 20)) {
-                ret_id += "-";
-            }
-            ret_id += (loop == 12 ? 4 : (loop == 16 ? (val & 3 | 8) : val)).toString(16);
-        }
-        this.id = ret_id;
-        return ret_id;
+        this.id = mofron..innerFunc.getId();
+        return this.id;
     }
     
     getTarget() {
