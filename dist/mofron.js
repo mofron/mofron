7d9dcdcc2a0c6f1a32d868625f4fd7e597ff8a73
simpart <simpart>
2016-12-22 16:55:19 +0900

    mod util.getId funcion
    change format uuid -> unix time

--- dist/mofron.js
+++ dist/mofron.js
@@ -139,17 +139,7 @@
 	mofron.util.getId = function (pf) {
 	    try {
 	        var _pf = pf === undefined ? "aid" : pf;
-	        var ret_id = _pf + "-";
-	        var loop = 0;
-	        var val = 0;
-	        for (loop = 0; loop < 32; loop++) {
-	            val = Math.random() * 16 | 0;
-	            if (loop === 8 || loop === 12 || loop === 16 || loop === 20) {
-	                ret_id += "-";
-	            }
-	            ret_id += (loop == 12 ? 4 : loop == 16 ? val & 3 | 8 : val).toString(16);
-	        }
-	        return ret_id;
+	        return _pf + '-' + new Date().getTime();
 	    } catch (e) {
 	        console.error(e.stack);
 	        throw new Error();
